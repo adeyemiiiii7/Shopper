2d076dd06531befed6f7738921189744f916ffe3
adeyemiiiii7 <aladesuyiadeyemi05@gmail.com>
2023-07-27 23:17:19 -0400

    add new item navigation

--- lib/main.dart
+++ lib/main.dart
@@ -16,11 +16,11 @@ class MyApp extends StatelessWidget {
         theme: ThemeData.dark().copyWith(
           useMaterial3: true,
           colorScheme: ColorScheme.fromSeed(
-            seedColor: const Color.fromARGB(255, 147, 229, 250),
+            seedColor: const Color.fromARGB(250, 2, 30, 36),
             brightness: Brightness.dark,
-            surface: const Color.fromARGB(255, 42, 51, 59),
+            surface: const Color.fromARGB(250, 2, 8, 15),
           ),
-          scaffoldBackgroundColor: const Color.fromARGB(255, 50, 58, 60),
+          scaffoldBackgroundColor: const Color.fromARGB(255, 0, 5, 6),
         ),
         home: const GroceryList());
   }
