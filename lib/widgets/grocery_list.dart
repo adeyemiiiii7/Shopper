dc90c23810783790c141f16d62e8a0c63880257b
adeyemiiiii7 <aladesuyiadeyemi05@gmail.com>
2023-08-01 21:53:26 -0400

    changes

--- lib/widgets/grocery_list.dart
+++ lib/widgets/grocery_list.dart
@@ -33,40 +33,48 @@ class _GroceryListState extends State<GroceryList> {
     final response = await http.get(url);
     //print(response.statusCode);
 
-    if (response.statusCode >= 400) {
+    try {
+      final url = Uri.https(
+          'shooper-58945-default-rtdb.firebaseio.com', 'shooper.json');
+      if (response.statusCode >= 400) {
+        setState(() {
+          _error = 'Failed to fetch data, pls try again later......';
+        });
+      }
+      if (response.body == 'null') {
+        setState(() {
+          _isLoading = false;
+        });
+        return;
+      }
+      //use json.decode to convert a map back
+      //add dynamic because it has numbers
+      final Map<String, dynamic> listData = json.decode(response.body);
+      final List<GroceryItem> loadedItems = [];
+      for (final item in listData.entries) {
+        final category = categories.entries
+            .firstWhere(
+                (catItem) => catItem.value.title == item.value['category'])
+            .value;
+        loadedItems.add(
+          GroceryItem(
+              id: item.key,
+              name: item.value['name'],
+              quantity: item.value['quantity'],
+              category: category),
+        );
+      }
+      //don't forget intialize with setstate
+      //to override the locally avaliable data of _groceryItems
       setState(() {
-        _error = 'Failed to fetch data, pls try again later......';
-      });
-    }
-    if (response.body == 'null') {
-      setState(() {
+        _groceryItems = loadedItems;
         _isLoading = false;
       });
-      return;
+    } catch (error) {
+      setState(() {
+        _error = 'Something Went Wrong!, Please Try Again Later.';
+      });
     }
-    //use json.decode to convert a map back
-    //add dynamic because it has numbers
-    final Map<String, dynamic> listData = json.decode(response.body);
-    final List<GroceryItem> loadedItems = [];
-    for (final item in listData.entries) {
-      final category = categories.entries
-          .firstWhere(
-              (catItem) => catItem.value.title == item.value['category'])
-          .value;
-      loadedItems.add(
-        GroceryItem(
-            id: item.key,
-            name: item.value['name'],
-            quantity: item.value['quantity'],
-            category: category),
-      );
-    }
-    //don't forget intialize with setstate
-    //to override the locally avaliable data of _groceryItems
-    setState(() {
-      _groceryItems = loadedItems;
-      _isLoading = false;
-    });
   }
 
   void _addItem() async {
