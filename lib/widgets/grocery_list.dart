7bc8233ad24eda0add06c03e1967c9a53d169373
adeyemiiiii7 <aladesuyiadeyemi05@gmail.com>
2023-08-02 15:19:21 -0400

    using future builder widgets

--- lib/widgets/grocery_list.dart
+++ lib/widgets/grocery_list.dart
@@ -16,65 +16,65 @@ class GroceryList extends StatefulWidget {
 
 class _GroceryListState extends State<GroceryList> {
   List<GroceryItem> _groceryItems = [];
-  var _isLoading = true;
+  // var _isLoading = true;
+  late Future<List<GroceryItem>> _loadedItems;
   String? _error;
 
   @override
   //to intialize the _loaditems method
   void initState() {
     super.initState();
-    _loadItems();
+    _loadedItems = _loadItems();
   }
 
-  void _loadItems() async {
+  Future<List<GroceryItem>> _loadItems() async {
     final url =
         Uri.https('shooper-58945-default-rtdb.firebaseio.com', 'shooper.json');
     //Uri.https('abc.firebaseio.com', 'shooper.json');
     final response = await http.get(url);
     //print(response.statusCode);
 
-    try {
-      final url = Uri.https(
-          'shooper-58945-default-rtdb.firebaseio.com', 'shooper.json');
-      if (response.statusCode >= 400) {
-        setState(() {
-          _error = 'Failed to fetch data, pls try again later......';
-        });
-      }
-      if (response.body == 'null') {
-        setState(() {
-          _isLoading = false;
-        });
-        return;
-      }
-      //use json.decode to convert a map back
-      //add dynamic because it has numbers
-      final Map<String, dynamic> listData = json.decode(response.body);
-      final List<GroceryItem> loadedItems = [];
-      for (final item in listData.entries) {
-        final category = categories.entries
-            .firstWhere(
-                (catItem) => catItem.value.title == item.value['category'])
-            .value;
-        loadedItems.add(
-          GroceryItem(
-              id: item.key,
-              name: item.value['name'],
-              quantity: item.value['quantity'],
-              category: category),
-        );
-      }
-      //don't forget intialize with setstate
-      //to override the locally avaliable data of _groceryItems
+    if (response.statusCode >= 400) {
       setState(() {
-        _groceryItems = loadedItems;
-        _isLoading = false;
-      });
-    } catch (error) {
-      setState(() {
-        _error = 'Something Went Wrong!, Please Try Again Later.';
+        _error = 'Failed to fetch data, pls try again later......';
       });
     }
+    if (response.body == 'null') {
+      //no need to set a laoding state manually any more thanks to future builder
+      // setState(() {
+      //   _isLoading = false;
+      // });
+      //return an empty list
+      return [];
+    }
+    //use json.decode to convert a map back
+    //add dynamic because it has numbers
+    //final category = categories.entries.firstWhere(...).value;: Inside the loop, this line looks up the category of the grocery item based on the 'category' property in the JSON data.
+    // It uses the categories.entries to get the list of key-value pairs in the categories map. Then, it uses the firstWhere method to find the first entry whose value's title matches the 'category' value of
+    //the current grocery item in the loop. Finally, it extracts the value of the matching category and assigns it to the category variable
+//loadedItems.add(...): After retrieving the category, this line creates a new Grocery Item
+    final Map<String, dynamic> listData = json.decode(response.body);
+    final List<GroceryItem> loadedItems = [];
+    for (final item in listData.entries) {
+      final category = categories.entries
+          .firstWhere(
+              (catItem) => catItem.value.title == item.value['category'])
+          .value;
+      loadedItems.add(
+        GroceryItem(
+            id: item.key,
+            name: item.value['name'],
+            quantity: item.value['quantity'],
+            category: category),
+      );
+    }
+    //don't forget intialize with setstate
+    //to override the locally avaliable data of _groceryItems
+    // setState(() {
+    //   _groceryItems = loadedItems;
+    //   _isLoading = false;
+    // });
+    return loadedItems;
   }
 
   void _addItem() async {
@@ -157,7 +157,8 @@ class _GroceryListState extends State<GroceryList> {
           ),
         ],
       ),
-      body: content,
+      body:
+          FutureBuilder(future: _loadedItems, builder: (context, snapshot) {}),
     );
   }
 }
